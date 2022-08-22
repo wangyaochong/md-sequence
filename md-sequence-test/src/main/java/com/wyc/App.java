e8271227dd3280f64b57c6e54dfaa2ae20e94dd8
wyc <wyc>
2022-08-22 20:33:15 +0800

    修改文件目录

---
+++ md-sequence-test/src/main/java/com/wyc/App.java
@@ -0,0 +1,13 @@
+package com.wyc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
